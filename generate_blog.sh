9184935cc58029762b8434b3c756f3be3014e05e
Govind <petrovgovind@gmail.com>
2024-03-17 23:44:23 +0100

    Cleanup directory after hugo setup

--- generate_blog.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-find . -maxdepth 1 -type f -name "*.md" -print
-# pandoc -s -f markdown --css pandoc.css {} -o {}.html
-find . -maxdepth 1 -type f -name "*.md" -print0 | xargs -0 -I {} sh -c 'pandoc --quiet -s --css basic.css -f markdown  -o $(echo {} | sed "s/\.md$/.html/") {}'
-# find . -type f -name "*.md" -print | xargs  -I {} sh -c "echo '{}'
-
